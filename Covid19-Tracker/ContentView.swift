fbd925dcf1cc7d377fc839acb1e3a3ac26df19a8
Regina <rlarcilla@hotmail.com>
2020-05-03 21:10:04 -0700

    Initial Commit

---
+++ Covid19-Tracker/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Covid19-Tracker
+//
+//  Created by Regina Arcilla on 2020-05-03.
+//  Copyright © 2020 Regina Arcilla. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
